4e0bc304b7ee3d12eb2c864f7420cdb601512b7c
St3veJ0bless <st3vej0bless@gmail.com>
2026-02-17 21:24:59 +0100

    Add files via upload

---
+++ novera-main/README.md
@@ -0,0 +1,5 @@
+Hi, thank you for purchasing this product from https://buzz.dev.
+
+Get support: https://www.buzz.dev/tickets/new
+Community Discord: https://discord.gg/buzz
+Tailwind Docs: https://tailwindcss.com/
---
+++ novera-main/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["ui-monospace", "SFMono-Regular", "Monaco", "Consolas", "Liberation Mono", "Menlo", "monospace"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
